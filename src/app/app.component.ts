166793da602a1fc4ab3914fe42bca301366a6e46
gayathri ethamukkala <egayathri20122000@gmail.com>
2023-12-30 16:18:19 +0530

    Event Management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import employeejson from 'src/assets/employee.json';
 
+interface employee{
+  id:number;
+  firstname:string;
+  lastname:string;
+  email:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  employee: employee[]=employeejson;
+  constructor() {
+    console.log(this.employee);
+  }
   title = 'Phase4Final';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent,
+    DeleteEmployeeComponent,
+    LoginComponent ,
   ],
   imports: [
     BrowserModule,
